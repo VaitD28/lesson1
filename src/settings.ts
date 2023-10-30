1e362e36da95951372bf73231ffb788ca0f750f4
VaitD28 <dimavoitkevich@gmail.com>
2023-10-30 11:31:23 +0300

    "AvailR  P"

--- src/settings.ts
+++ src/settings.ts
@@ -19,13 +19,13 @@ type ErrorType = {                                       // Типизируем
 
 enum AvailableResolutions {                              // Типизируем AvailableResolutions
     P144='P144',
-    P240='P144',
-    P360='P144',
+    P240='P240',
+    P360='P360',
     P480='P480',
-    P720='P480',
-    P1080='P480',
+    P720='P720', 
+    P1080='P1080',
     P1440='1440',
-    P2160='P1440'
+    P2160='P2160'
 }
 
 type VideoType = {                                      // Типизируем Video 
